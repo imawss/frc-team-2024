491331ffda456a46e285ee06a3d33693b8014f01
Aras ARKAN <arasarkan9@gmail.com>
2024-01-30 18:03:40 +0300

    commit for merge

--- src/main/java/frc/robot/subsystems/LauncherSubsystem.java
+++ src/main/java/frc/robot/subsystems/LauncherSubsystem.java
@@ -22,11 +22,6 @@ public class LauncherSubsystem extends SubsystemBase {
     rightCIMMotor = new PWMSparkMax(Constants.LaunchersConstants.RIGHT_CIM_MOTOR);
   }
 
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
   public Command launchCommand() {
     
     throttle(leftCIMMotor, launcherController.getRightTriggerAxis());
@@ -44,13 +39,6 @@ public class LauncherSubsystem extends SubsystemBase {
     motor.set(gasValue);
   }
 
-  
-
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
   public boolean exampleCondition() {
     // Query some boolean state, such as a digital sensor.
     return false;
